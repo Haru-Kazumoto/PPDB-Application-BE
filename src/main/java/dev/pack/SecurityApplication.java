c54d8143e24480b7b8f60ecf8520fcee3a1ff8ca
Ikhsan892 <mfatihul8902@gmail.com>
2023-11-05 21:50:24 +0700

    refactor : filestorage service

--- src/main/java/dev/pack/SecurityApplication.java
+++ src/main/java/dev/pack/SecurityApplication.java
@@ -1,19 +1,12 @@
 package dev.pack;
 
-import dev.pack.services.FilesStorageService;
+import dev.pack.filestorage.FilesStorageService;
 import jakarta.annotation.Resource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableTransactionManagement
