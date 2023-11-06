680573a0ecaf9f02763626ffccb53b3bc3786cb8
Ikhsan892 <mfatihul8902@gmail.com>
2023-11-06 08:31:40 +0700

    refactor : student achievements service

--- src/main/java/dev/pack/modules/student/StudentServiceImpl.java
+++ src/main/java/dev/pack/modules/student/StudentServiceImpl.java
@@ -45,6 +45,7 @@ public class StudentServiceImpl implements StudentService{
     private final StudentPaymentRepository studentPaymentRepository;
     private final FilesStorageService filesStorageService;
     private final StudentUtils studentUtils;
+    private final StudentAchievementRepository studentAchievementRepository;
 
     @Override
     @Transactional
@@ -243,6 +244,12 @@ public class StudentServiceImpl implements StudentService{
                         .build()
         );
 
+    }
+
+    @Override
+    public List<StudentAchievement> getStudentAchievement(GetStudentAchievementDto dto) {
+        Student student = this.studentRepository.findById(dto.getStudentId()).orElseThrow(() -> new DataNotFoundException("Data siswa tidak ditemukan"));
+        return this.studentAchievementRepository.getStudentAchievementByStudentAndType(student,dto.getType());
     }
 
     public StudentLogs printCard(PrintCardDto printCardDto) {
