155d8a8a8a0ec9cd1303fbfc14bf733ddf337077
abay bay <akbar25005@gmail.com>
2023-12-01 09:59:12 +0700

    feat: getallStaging

--- src/main/java/dev/pack/modules/staging/StagingRepository.java
+++ src/main/java/dev/pack/modules/staging/StagingRepository.java
@@ -16,34 +16,33 @@ import java.util.Optional;
 @Repository
 public interface StagingRepository extends JpaRepository<Staging, Integer> {
 
-
     @Query("""
-        select s from Staging s where s.name like CONCAT('%', CONCAT(:name, '%'))
-""")
+                    select s from Staging s where s.name like CONCAT('%', CONCAT(:name, '%'))
+            """)
     Optional<Staging> findByName(String name);
 
     @Query("""
-        select s from Staging s where s.name like CONCAT('%', CONCAT(:name, '%')) and s.type = :type and s.grade = :grade
-""")
+                    select s from Staging s where s.name like CONCAT('%', CONCAT(:name, '%')) and s.type = :type and s.grade = :grade
+            """)
     Optional<Staging> findByNameAndStagingType(String name, FormPurchaseType type, Grade grade);
 
     @Query(value = """
-        select
-            s.id as id,
-            s.name as name,
-            s.index as index,
-            (case when
-                (
-                    select max(d.id) from student_logs d where d.student_id = :student_id and d.staging_id = s.id
-                )  is null
-            then 0
-            else 1
-            end) as is_done
-        from staging s
-        where s.type = :type
-        order by s.index asc
-    """,nativeQuery = true)
-    List<GetAllStaging> findStudentOffset(Integer student_id,Integer type);
-
+                select
+                    s.id as id,
+                    s.name as name,
+                    s.index as index,
+                    (case when
+                        (
+                            select max(d.id) from student_logs d where d.student_id = :student_id and d.staging_id = s.id
+                        )  is null
+                    then 0
+                    else 1
+                    end) as is_done,
+                    s.grade as grade
+                from staging s
+                where s.type = :type
+                order by s.index asc
+            """, nativeQuery = true)
+    List<GetAllStaging> findStudentOffset(Integer student_id, Integer type);
 
 }
